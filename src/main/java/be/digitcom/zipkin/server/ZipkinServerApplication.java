c8ec1ac31530680544ae322b0aa67a9a11380d13
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-13 18:02:29 +0200

    Initial state

---
+++ src/main/java/be/digitcom/zipkin/server/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.zipkin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/digitcom/zipkin/server/ZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitcom.zipkin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
